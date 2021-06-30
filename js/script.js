d5ddec30ca67a039d885dca8710b880f39bf6d08
sergeycolesnik1 <sergey19-77@ukr.net>
2021-06-30 15:50:46 +0300

    str2

--- js/script.js
+++ js/script.js
@@ -12,36 +12,40 @@ let newLs3 = document.querySelector('.news__article-list3');
 let newLs4 = document.querySelector('.news__article-list4');
 
 
-console.log(newLs4);
+console.log(abBx);
 
 
 function Switching() {
     swBt.onclick = () => {
-        swBox2.style.opacity = 1;
-        arLs.style.opacity = 0;
-        arLs2.style.opacity = 1;
-        abBx2.style.opacity = 1;
-        abBx.style.opacity = 0;
-        swBox.style.opacity= 0;
+        swBox2.style.display = 'block';
+        arLs.style.display = 'none';
+        arLs2.style.display = 'block';
+        abBx2.style.display = 'block';
+        abBx.style.display = 'none';
+        abBx2.style.zIndex=4;
+        abBx.style.zIndex=0;
+        swBox.style.display = 'none';
         swBt.style.opacity = 0;
         swBt2.style.opacity = 1;
-        newLs3.style.opacity = 0;
-        newLs4.style.opacity = 1;
+        newLs3.style.display='none';
+        newLs4.style.display='block';
         swBt.style.zIndex = 0;
         swBt2.style.zIndex = 4;
     }
 
     swBt2.onclick = () => {
-        swBox2.style.opacity = 0;
-        arLs.style.opacity = 1;
-        arLs2.style.opacity = 0;
-        swBox.style.opacity = 1;
-        abBx2.style.opacity = 0;
-        abBx.style.opacity = 1;
+        swBox2.style.display = 'none';
+        arLs.style.display = 'block';
+        arLs2.style.display = 'none';
+        swBox.style.display = 'block';
+        abBx2.style.display = 'none';
+        abBx.style.display = 'block';
+        abBx2.style.zIndex = 0;
+        abBx.style.zIndex = 4;
         swBt.style.opacity = 1;
         swBt2.style.opacity = 0;
-        newLs3.style.opacity = 1;
-        newLs4.style.opacity = 0;
+        newLs3.style.display = 'block';
+        newLs4.style.display = 'none';
         swBt.style.zIndex = 4;
         swBt2.style.zIndex = 0;
     }
@@ -53,8 +57,10 @@ Switching();
 //---------------кнопка вывода обомне---------------
 function autobiography() {
     let abBt = document.querySelector('.v-bt2');
+    let abBt1 = document.querySelector('.v-bt1');
+
     let man = document.querySelector('.main');
-    
+
     abBt.onclick = function () {
         if (man.style.display == "none") {
             man.style.display = "block";
@@ -64,5 +70,16 @@ function autobiography() {
       
         console.log(abBt);
     }
+
+    abBt1.onclick = function () {
+        if (man.style.display == "none") {
+            man.style.display = "block";
+        } else {
+            man.style.display = "none";
+        }
+
+        console.log(abBt1);
+    }
 }
+
 autobiography();
